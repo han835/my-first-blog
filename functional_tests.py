7957a8844c706786f0178cc4429b39ef98b7b112
Harry Neil <harryneil.icm@gmail.com>
2020-05-20 14:36:47 +0100

    Updating functional tests with comments and changing format

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Blog' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_look_at_blog(self):
+        # I want to look at my website.
+        # I go to check it out.
+        self.browser.get('http://127.0.0.1:8000')
+
+        # I should be able to view all posts initially.
+        # I should be able to view a post in detail.
+        # I should be able to edit a post.
+        # I should be able to create a post.
+
+        # The title should mention my name.
+        self.assertIn('Harry', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
