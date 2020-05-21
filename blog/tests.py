6f5254bdeb0c3eef0a6113d73793edbc59f1d126
Harry Neil <harryneil.icm@gmail.com>
2020-05-21 10:59:50 +0100

    Completed first homepage unit test

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,8 @@
+from django.urls import resolve
 from django.test import TestCase
+from blog.views import post_list
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_post_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
