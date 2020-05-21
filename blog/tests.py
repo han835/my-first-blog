4abb90294bdfcd3159395305df5ede3ece5d291c
Harry Neil <harryneil.icm@gmail.com>
2020-05-21 11:57:40 +0100

    Updated unit tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,18 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from blog.views import post_list
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_post_view(self):
         found = resolve('/')
         self.assertEqual(found.func, post_list)
+
+    def test_post_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<html>', html)
+        self.assertIn('<title>Harry\'s Blog</title>', html)
+        self.assertIn('</html>', html)
